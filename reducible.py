d5d743a868f13d0e3c5e229fffcf2546014528c5
apathi2004 <apathi@utexas.edu>
2025-04-05 22:49:45 -0500

    Add files via upload

--- reducible.py
+++ reducible.py
@@ -106,6 +106,8 @@ def find_word(s, hash_table):
     pre: s is a string, and hash_table is a list representing the hash table.
     post: Returns True if s is found in hash_table, otherwise returns False.
     """
+    if hash_table == []:
+        return False
     index = hash_word(s, len(hash_table))
     size = step_size(s)
     table_size = len(hash_table)
@@ -113,11 +115,13 @@ def find_word(s, hash_table):
     if hash_table[index] == s:
         return True
     while hash_table[index] != "":
-        index = (index + size) % table_size
         if hash_table[index] == s:
             return True
+        index = (index + size) % table_size
         if index == og_index:
             return False
+
+
 
 def is_reducible(s, hash_table, hash_memo):
     """
